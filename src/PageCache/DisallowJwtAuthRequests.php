213ad3d573c76757a9493b04fd6938a5d646b302
Jonathan Green <jonathan.green@lyrasis.org>
2017-02-09 16:55:12 -0400

    Prevent caching response in GETs using JWT header
    Implement Drupal RequestPolicyInterface to prevent get responses from
    being cached when using jwt_auth. Before making a get request would
    cache and another request without the token would be served up the same
    content.
    
    Fixes issue 2791127:
    https://www.drupal.org/node/2791127

---
+++ src/PageCache/DisallowJwtAuthRequests.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\jwt\PageCache;
+
+use Drupal\Core\PageCache\RequestPolicyInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Cache policy for pages served from JWT auth.
+ *
+ * This policy disallows caching of requests that use jwt_auth for security
+ * reasons. Otherwise responses for authenticated requests can get into the
+ * page cache and could be delivered to unprivileged users.
+ */
+class DisallowJwtAuthRequests implements RequestPolicyInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function check(Request $request) {
+    $auth = $request->headers->get('Authorization');
+    if(preg_match('/^Bearer .+/', $auth)) {
+      return self::DENY;
+    }
+  }
+
+}
