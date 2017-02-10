f25a4f8bb64cdfbe0373c257fa9c8d1dfe2eadcc
Jonathan Green <jonathan.green@lyrasis.org>
2017-02-10 17:03:21 -0400

    Add support for additional algorithms
    JWT module now supports all the algorithms supported by the underlying
    firebase JWT library. This includes the asymetric RSA algorithm RSA256.
    The algorithm is set in the configruation page.
    
    In order to support both asymmetric and symmetric algorithms, a couple
    new key types have been defined for the key module. This lets us use PEM
    encoded private keys for RSA encryption.

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\jwt\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\key\KeyRepositoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\jwt\Transcoder\JwtTranscoder;
 
 /**
  * Class ConfigForm.
@@ -11,6 +15,40 @@ use Drupal\Core\Form\FormStateInterface;
  * @package Drupal\jwt\Form
  */
 class ConfigForm extends ConfigFormBase {
+
+  protected $transcoder;
+  protected $keyRepo;
+
+  /**
+   * ConfigForm constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config factory for parent.
+   * @param \Drupal\key\KeyRepositoryInterface $key_repo
+   *   Key repo to validate keys.
+   * @param \Drupal\jwt\Transcoder\JwtTranscoder $transcoder
+   *   JWT Transcoder.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    KeyRepositoryInterface $key_repo,
+    JwtTranscoder $transcoder
+  ) {
+    $this->keyRepo = $key_repo;
+    $this->transcoder = $transcoder;
+    parent::__construct($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('key.repository'),
+      $container->get('jwt.transcoder')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -29,13 +67,68 @@ class ConfigForm extends ConfigFormBase {
   }
 
   /**
+   * AJAX Function callback.
+   *
+   * @param array $form
+   *   Drupal form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Drupal formstate object.
+   *
+   * @return mixed
+   *   Updated AJAXed form.
+   */
+  public function ajaxCallback(array &$form, FormStateInterface $form_state) {
+    return $form['key-container'];
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['jwt_key'] = [
+    $form['key-container'] = [
+      '#type' => 'container',
+      '#prefix' => '<div id="jwt-key-container">',
+      '#suffix' => '</div>',
+      '#weight' => 10,
+    ];
+
+    $form['jwt_algorithm'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Algorithm'),
+      '#options' => $this->transcoder->getAlgorithmOptions(),
+      '#ajax' => [
+        'callback' => '::ajaxCallback',
+        'event' => 'change',
+        'wrapper' => 'jwt-key-container',
+        'progress' => [
+          'type' => 'throbber',
+        ],
+      ],
+      '#default_value' => $this->config('jwt.config')->get('algorithm'),
+    ];
+
+    if ($form_state->isValueEmpty('jwt_algorithm')) {
+      if (!empty($this->config('jwt.config')->get('algorithm'))) {
+        $type = $this->transcoder->getAlgorithmType($this->config('jwt.config')->get('algorithm'));
+      }
+      else {
+        $type = 'jwt_hs';
+      }
+    }
+    else {
+      $type = $this->transcoder->getAlgorithmType($form_state->getValue('jwt_algorithm'));
+    }
+    $text = ($type == 'jwt_hs') ? $this->t('Secret') : $this->t('Private Key');
+
+    $form['key-container']['jwt_key'] = [
       '#type' => 'key_select',
-      '#title' => $this->t('JWT Secret'),
+      '#title' => $text,
       '#default_value' => $this->config('jwt.config')->get('key_id'),
+      '#key_filters' => [
+        'type' => $type,
+      ],
+      '#validated' => TRUE,
+      '#required' => TRUE,
     ];
 
     return parent::buildForm($form, $form_state);
@@ -45,6 +138,18 @@ class ConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $algorithm = $form_state->getValue('jwt_algorithm');
+    $key_id = $form_state->getValue('jwt_key');
+    $key = $this->keyRepo->getKey($key_id);
+
+    if ($key != NULL && $key->getKeyType()->getPluginId() != $this->transcoder->getAlgorithmType($algorithm)) {
+      $form_state->setErrorByName('jwt_key', $this->t('Incorrect key type selected.'));
+    }
+
+    if ($key != NULL && $key->getKeyType()->getConfiguration()['algorithm'] != $algorithm) {
+      $form_state->setErrorByName('jwt_key', $this->t('Key does not match algorithm selected.'));
+    }
+
     parent::validateForm($form, $form_state);
   }
 
@@ -55,6 +160,10 @@ class ConfigForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);
 
     $values = $form_state->getValues();
+
+    if (isset($values['jwt_algorithm'])) {
+      $this->config('jwt.config')->set('algorithm', $values['jwt_algorithm'])->save();
+    }
 
     if (isset($values['jwt_key'])) {
       $this->config('jwt.config')->set('key_id', $values['jwt_key'])->save();
--- src/Transcoder/JwtTranscoder.php
+++ src/Transcoder/JwtTranscoder.php
@@ -25,16 +25,67 @@ class JwtTranscoder implements JwtTranscoderInterface {
   /**
    * The allowed algorithms with which a JWT can be decoded.
    *
-   * @var array
+   * @var string
    */
-  protected $algorithms = array('HS256');
+  protected $algorithm;
+
+  /**
+   * The algorithm type we are using.
+   *
+   * @var string
+   */
+  protected $algorithmType;
 
   /**
    * The key used to encode/decode a JsonWebToken.
    *
    * @var string
    */
-  protected $secret;
+  protected $secret = NULL;
+
+  /**
+   * The PEM encoded private key used for signing RSA JWTs.
+   *
+   * @var string
+   */
+  protected $privateKey = NULL;
+
+  /**
+   * The PEM encoded public key used to verify signatures on RSA JWTs.
+   *
+   * @var string
+   */
+  protected $publicKey = NULL;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getAlgorithmOptions() {
+    return [
+      'HS256' => 'HMAC using SHA-256 (HS256)',
+      'HS384' => 'HMAC using SHA-384 (HS384)',
+      'HS512' => 'HMAC using SHA-512 (HS512)',
+      'RS256' => 'RSASSA-PKCS1-v1_5 using SHA-256 (RS256)',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getAlgorithmType($algorithm) {
+    switch ($algorithm) {
+      case 'HS256':
+      case 'HS384':
+      case 'HS512':
+        return 'jwt_hs';
+
+      case 'RS256':
+        return 'jwt_rs';
+
+      default:
+        return NULL;
+    }
+  }
 
   /**
    * Constructs a new JwtTranscoder.
@@ -48,14 +99,21 @@ class JwtTranscoder implements JwtTranscoderInterface {
    */
   public function __construct(JWT $php_jwt, ConfigFactoryInterface $configFactory, KeyRepositoryInterface $key_repo) {
     $this->transcoder = $php_jwt;
-
     $key_id = $configFactory->get('jwt.config')->get('key_id');
+    $this->setAlgorithm($configFactory->get('jwt.config')->get('algorithm'));
+
     if (isset($key_id)) {
       $key = $key_repo->getKey($key_id);
-
       if (!is_null($key)) {
-        $secret = $key->getKeyValue();
-        $this->setSecret($secret);
+        $key_value = $key->getKeyValue();
+        if ($this->algorithmType == 'jwt_hs') {
+          // Symmetric algorithm so we set the secret.
+          $this->setSecret($key_value);
+        }
+        elseif ($this->algorithmType == 'jwt_rs') {
+          // Asymmetric algorithm so we set the private key.
+          $this->setPrivateKey($key_value);
+        }
       }
     }
   }
@@ -70,9 +128,51 @@ class JwtTranscoder implements JwtTranscoderInterface {
   /**
    * {@inheritdoc}
    */
+  public function setAlgorithm($algorithm) {
+    $this->algorithm = $algorithm;
+    $this->algorithmType = $this->getAlgorithmType($algorithm);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPrivateKey($private_key, $derive_public_key = TRUE) {
+    $key_context = openssl_pkey_get_private($private_key);
+    $key_details = openssl_pkey_get_details($key_context);
+    if ($key_details === FALSE || $key_details['type'] != OPENSSL_KEYTYPE_RSA) {
+      return FALSE;
+    }
+
+    $this->privateKey = $private_key;
+    if ($derive_public_key) {
+      $this->publicKey = $key_details['key'];
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPublicKey($public_key) {
+    $key_context = openssl_pkey_get_public($public_key);
+    $key_details = openssl_pkey_get_details($key_context);
+    if ($key_details === FALSE || $key_details['type'] != OPENSSL_KEYTYPE_RSA) {
+      return FALSE;
+    }
+
+    $this->publicKey = $public_key;
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function decode($jwt) {
+    $key = $this->getKey('decode');
+    $algorithms = [$this->algorithm];
     try {
-      $token = $this->transcoder->decode($jwt, $this->secret, $this->algorithms);
+      $token = $this->transcoder->decode($jwt, $key, $algorithms);
     }
     catch (\Exception $e) {
       throw JwtDecodeException::newFromException($e);
@@ -83,28 +183,38 @@ class JwtTranscoder implements JwtTranscoderInterface {
   /**
    * {@inheritdoc}
    */
-  public function encode(JsonWebTokenInterface $jwt, array $options = []) {
-    $options = $this->getOptions($options);
-    $encoded = $this->transcoder->encode($jwt->getPayload(), $options['key'], $options['alg']);
+  public function encode(JsonWebTokenInterface $jwt) {
+    $key = $this->getKey('encode');
+    // Refuse to encode if we don't have a key yet.
+    if ($key === NULL) {
+      return FALSE;
+    }
+    $encoded = $this->transcoder->encode($jwt->getPayload(), $key, $this->algorithm);
     return $encoded;
   }
 
   /**
-   * Gets a standard set of options for encoding a JWT, with overrides.
+   * Helper function to get the correct key based on operation.
    *
-   * @param array $options
-   *   Additional options.
+   * @param string $operation
+   *   The operation being performed. One of: encode, decode.
    *
-   * @return array
-   *   Complete set of options.
+   * @return null|string
+   *   Returns NULL if opteration is not found. Otherwise returns key.
    */
-  protected function getOptions(array $options = array()) {
-    $defaults = array(
-      'alg' => 'HS256',
-      'key' => $this->secret,
-    );
-
-    return array_merge_recursive($options, $defaults);
+  protected function getKey($operation) {
+    if ($this->algorithmType == 'jwt_hs') {
+      return $this->secret;
+    }
+    elseif ($this->algorithmType == 'jwt_rs') {
+      if ($operation == 'encode') {
+        return $this->privateKey;
+      }
+      elseif ($operation == 'decode') {
+        return $this->publicKey;
+      }
+    }
+    return NULL;
   }
 
 }
